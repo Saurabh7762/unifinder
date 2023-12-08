df5ec0260d1bb3c385c1717db422410658657e22
Saurabh7762 <Saurabhkumars7762@gmail.com>
2023-12-09 04:58:47 +0530

    remove http: for API Url

--- src/API/universityApi.js
+++ src/API/universityApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const fetchUniversities = async (searchUniversity) => {
   try {
     const response = await axios.get(
-      `http://universities.hipolabs.com/search?name=${searchUniversity}`
+      `//universities.hipolabs.com/search?name=${searchUniversity}`
     );
     return response.data;
   } catch (error) {
